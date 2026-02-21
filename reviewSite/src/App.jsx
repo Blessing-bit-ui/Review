4c14a21742af42eb558523bf6343ef0974ddb6bd
Blessing-bit-ui <yembeblessing27@gmail.com>
2026-02-21 07:45:42 -0800

    created a function Header

--- reviewSite/src/App.jsx
+++ reviewSite/src/App.jsx
@@ -4,13 +4,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div class=" margin-0 padding-0 bg-green-500 w-screen h-screen">
-      <div>ggggg</div>
+    <div className="bg-green-500 w-screen h-screen ">
+      <div>
+       <Header/>
+      </div>
+    </div>
+  );
+}
+export default App
+
+
+function Header(){
+  return (
+    <div>
+      <div className="shadow-[0_5px_0_rgba(0,0,0,0.4)] bg-white p-2 flex justify-between">
+        <div>
+          <h1 className="text-[30px] text-green-700 font-bold">
+            African Business Directory
+          </h1>
+          <p class="text-green-700 font-bold">
+            Shining a spotLight on African Businesses
+          </p>
+        </div>
+        <div className="flex justify-around w-6/12">
+          <p>Write a review</p>
+          <p> Industries</p>
+          <p>About us</p>
+          <p>Login</p>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App
+
+
+
