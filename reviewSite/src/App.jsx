706fae6790d90f707f22f5a73df91de812ce6156
Blessing-bit-ui <yembeblessing27@gmail.com>
2026-02-21 05:14:43 -0800

    install tailwind and react router

---
+++ reviewSite/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div class=" margin-0 padding-0 bg-green-500 w-screen h-screen">
+      <div>ggggg</div>
+    </div>
+  );
+}
+
+export default App
