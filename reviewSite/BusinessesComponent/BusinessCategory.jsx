62b7b7c1ebac0c8384693db9bbcc92ba48eedcc9
Blessing-bit-ui <yembeblessing27@gmail.com>
2026-08-22 07:48:36 -0700

    ADDED STYLING  ON THE BUSINESS CATEGORY PAGE AND CREATED A STEP AND SEARHBUSINESS STATE

--- reviewSite/BusinessesComponent/BusinessCategory.jsx
+++ reviewSite/BusinessesComponent/BusinessCategory.jsx
@@ -1,3 +1,4 @@
+
 import { useParams } from "react-router-dom"
 import { useBusiness } from "../ContextProvider/BusinessProvider"
 import { useState, useEffect } from "react"
@@ -6,52 +7,60 @@ import { useAuth } from "../ContextProvider/AuthProvider"
 import { NavLink } from "react-router-dom"
 
 function BusinessCategory() {
+  const { searchbusinee, setSearchBusiness } = useState(""); // to search business
+  const { step, setStep } = useState(+1); // Will use this to create a function which
+  // onClick i will be able to move to the next business
+  const { category } = useParams();
+  const { businesses, getCategories, currentCategory } = useBusiness();
+  const navigate = useNavigate();
+  const { forceLogin } = useAuth();
+  //const businessCategory = businesses.find((bus)=> bus.category === category)
 
-const {category} = useParams()
-const { businesses, getCategories, currentCategory } = useBusiness();
-const navigate = useNavigate()
-const { forceLogin } = useAuth();
-//const businessCategory = businesses.find((bus)=> bus.category === category)
-
-/*useEffect(function(){
+  /*useEffect(function(){
 getCategories(category)
 }, [category])*/
 
-useEffect(() => {
-  if (category)getCategories(category);
-},[category]);
+  useEffect(() => {
+    if (category) getCategories(category);
+  }, [category]);
 
-    return (
-      <div>
-        <div className="shadow-[0_5px_0_rgba(0,0,0,0.4)] bg-white p-2 flex justify-between items-center ">
-          <div>
-            <h1 className="text-[30px] text-lime-700 font-bold">
-              African Business Directory
-            </h1>
-
-          </div>
-        </div>
-        <div className="bg-lime-600 w-screen h-screen">
-          <h1 class="text-[30px] text-white font-bold ">
-            Businesses in {category} category
+  return (
+    <div>
+      <div className="shadow-[0_5px_0_rgba(0,0,0,0.4)] bg-white p-2 flex justify-between items-center ">
+        <div>
+          <h1 className="text-[30px] text-lime-700 font-bold">
+            African Business Directory
           </h1>
-          <ul>
+        </div>
+      </div>
+      <div className="bg-lime-600 w-screen h-screen ml-4">
+        <h1 class="text-[30px] text-white font-bold  ">
+          Businesses in {category} category
+        </h1>
+        <ul>
+          <div className="flex justify-between gap-1">
             {currentCategory.map((curr) => (
-              <div key={curr.id}>
+              <div
+                key={curr.id}
+                className="bg-white p-3 border rounded-lg  w-[250px]"
+              >
                 <NavLink to={`/name/${curr.name}`}>
-                  <h1>{curr.name}</h1>
+                  <h1 className="text-red-900 font-bold">{curr.name}</h1>
                   <h2>{curr.email}</h2>
                   <h1>
                     Location {curr.country} <span>{curr.city}</span>
                   </h1>
-                  <p>Write a review</p>
+                  <p className="underline decoration-pink-900 hover:text-green-600 hover:decoration-green-600">
+                    Write a review
+                  </p>
                 </NavLink>
               </div>
             ))}
-          </ul>
-        </div>
+          </div>
+        </ul>
       </div>
-    );
+    </div>
+  );
 }
 
 export default BusinessCategory
