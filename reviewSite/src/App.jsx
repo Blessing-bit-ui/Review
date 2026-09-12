428d221430070178ae352667dd3324671560b942
Blessing-bit-ui <yembeblessing27@gmail.com>
2026-09-12 07:44:56 -0700

    updated css and import map

--- reviewSite/src/App.jsx
+++ reviewSite/src/App.jsx
@@ -17,6 +17,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import { NavLink } from 'react-router-dom';
 import './App.css'
 import ProtectedApp from '../ProtectedApp';
+import Africanmap from "./assets/Africanmap.png";
 
 function App() {
   return (
@@ -68,18 +69,27 @@ export default App
 
 function Home(){
   return (
-    <div className="bg-[#064e3b]">
-      <div className="shadow-[0_10px_15px_rgba(0,0,0,0.4)] bg-[#020617]">
+    <div className="bg-rgb(255,255,255)">
+      <div className=" relative shadow-[0_2px_2px_rgba(0,0,0,0.4)] bg-[#020617] md:h-[150px]">
         <Header />
-        <Vision />
+        <div className="absolute bottom-2 left-0 w-full">
+          <Discover />
+        </div>
       </div>
       <div className=" w-screen h-screen mt-1">
+        
+          <Vision />
+    
+
         <FindBusiness />
         <BusinessCategoriesList />
       </div>
     </div>
   );
 }
+
+
+
 
 function Header(){
 const { Login, auth } = useAuth();
@@ -90,7 +100,6 @@ const { Login, auth } = useAuth();
           <h1 className="text-[30px] text-[#ffff] font-[PT_Sans] font-bold">
             African Business Directory
           </h1>
-          <p class="text-green-700 font-bold">Discover Africa's MarketPlace</p>
         </div>
         <div className="flex justify-around w-6/12">
           <NavLink
@@ -107,7 +116,7 @@ const { Login, auth } = useAuth();
           <UserSignUp />
           <div>
             {auth ? (
-              <p className="text-[#ffff]">Welcome User</p>
+              <p className="text-[#ffffff]">Welcome User</p>
             ) : (
               <NavLink
                 to="/login"
@@ -122,13 +131,32 @@ const { Login, auth } = useAuth();
     </div>
   );
 }
+function Discover(){
+  return (
+    <div>
+      <p className="text-green-700 font-bold text-center">
+        Discover Africa's MarketPlace
+      </p>
+    </div>
+  );
+}
 
 function Vision(){
-  return(
-<div className="text-[40px] text-center text-white font-bold  mr-auto ml-auto">
- <h1> Shining a Spotlight on African Businesses to Increase Visibility,<br/>Build Credibility and Foster Trust.</h1>
-</div>
-  )
+  return (
+    <div className="flex w-11/12 justify-between items-center">
+      <div>
+        <h1 className="text-[50px]  text-[#22c55e]  w-1\12 font-[Rubik] tracking-[-2pt] leading-tight font-style: italic">
+          Shining a Spotlight <br /> on African Businesses <br /> to Increase
+          Visibility,
+          <br />
+          Build Credibility <br /> and Foster Trust.
+        </h1>
+      </div>
+      <div>
+        <img src={Africanmap} className="w-[400px] h-[400px]" />
+      </div>
+    </div>
+  );
 }
 
 function RegisterBusiness(){
